d8213e55276a1d5fb63fee7c30621d7333259ec7
aviehu <aviehu@gmail.com>
2018-08-16 14:21:22 +0300

    sads

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// server
 var express = require("express");
 var app = express();
-var path = require("path");
+app.set("port", process.env.PORT || 5000);
 
+// Our first route
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.send("Hello Node + GitHub!");
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+// Listen to port
+app.listen(app.get("port"), function() {
+  console.log("App is listening on port " + app.get("port"));
+});
