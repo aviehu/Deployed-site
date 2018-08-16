c2f44943c211a9d1463feaca61b9f87421947c71
aviehu <aviehu@gmail.com>
2018-08-16 14:14:55 +0300

    qwe

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// server
 var express = require("express");
 var app = express();
-app.set("port", process.env.PORT || 5000);
+var path = require("path");
 
-// Our first route
 app.get("/", function(req, res) {
-  res.send("Hello Node + GitHub!");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-// Listen to port
-app.listen(app.get("port"), function() {
-  console.log("App is listening on port " + app.get("port"));
-});
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
