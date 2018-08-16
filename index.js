97460382a54f95b0df225407d22ac894660b8e52
aviehu <aviehu@gmail.com>
2018-08-16 15:08:43 +0300

    maybe this time

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set("port", process.env.PORT || 5000);
 
 // Our first route
 app.get("/", function(req, res) {
-  res.send(path("./index.html"));
+  res.sendfile(Path("./index.html"));
 });
 
 // Listen to port
