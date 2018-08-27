71b8ed53cde4de51aeeab524215d4b53c46ac840
aviehu <aviehu@gmail.com>
2018-08-27 14:07:30 +0300

    Eslint fixes + remove node modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "airbnb-base"
 };
